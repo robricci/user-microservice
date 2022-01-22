17f23c54afc7ef22bbac3eea943e1ab485b9a956
robricci <ricci_roberto@icloud.com>
2022-01-22 09:46:22 +0100

    Add UserApplication Unit Test

---
+++ src/test/java/it/unisannio/UserApplicationTests.java
@@ -0,0 +1,20 @@
+package it.unisannio;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.util.Assert.*;
+
+
+@SpringBootTest
+public class UserApplicationTests {
+
+
+    @Test
+    public void contextLoads() {
+
+        assertTrue(true);
+    }
+
+
+}
